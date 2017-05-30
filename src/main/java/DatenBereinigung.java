928aeaf88eb0876f7384a5fac29e38d4109fbbd0
trofimovamw <trofimovamw@gmail.com>
2017-05-30 23:01:16 +0200

    second commit

--- src/main/java/DatenBereinigung.java
+++ src/main/java/DatenBereinigung.java
@@ -20,13 +20,13 @@ public class DatenBereinigung {
 
     public static String candidate;
     public static String text;
-    //public static boolean isRetweet;
-    //public static String original;
+    public static String originalAuthor;
     public static String time;
-    public static boolean quote;
+    public static String isRetweet;
     public static String retweet;
     public static String favorite;
     public static List<String> hashtags = new ArrayList<String>();
+    public static String damaged_line;
 
 
     public DatenBereinigung(String filename) {}
@@ -47,25 +47,28 @@ public class DatenBereinigung {
     }
 
     public static void checkTweet(String str) {
-        text = str;
+        String txt = str.replaceAll("[^\\p{ASCII}]", " ");
+        text = txt;
     }
 
+    //Format 2016-09-27T21:25:31
+
     public static void checkTime(String str) {
-        if (str.length() == 19) {
+        if (str.matches("^\\d{4}[-]{1}\\d{2}[-]{1}\\d{2}[T]{1}\\d{2}[:]{1}\\d{2}[:]{1}\\d{2}") == true) {
             time = str;
         } else {
             time = " ";
         }
     }
 
-    public static void checkQuote(String str) {
-        if (str == "True") {
-            quote = true;
-        } else if (str == "False") {
-            quote = false;
-        } else {
-            quote = false;
-        }
+    public static void checkIsRetweet(String str) {
+        isRetweet = str;
+    }
+
+    public static void checkOriginal(String str) {
+
+        originalAuthor = str;
+
     }
 
     public static void checkRetweetCount(String count) {
@@ -85,13 +88,20 @@ public class DatenBereinigung {
         List<String> htags = new ArrayList<String>();
 
         while (m.find()) {
-            String hashtag = m.group(1);
-            System.out.println(hashtag);
+            String hashtag = m.group(1).replace("#","");
+
+            //System.out.println(hashtag);
+            //"#1" not a hashtag?
             htags.add(hashtag);
         }
 
         hashtags = htags;
     }
+
+    public static void writeHeader(PrintStream out) {
+        out.println("handle;text;is_retweet;original_author;hashtags;time;retweet_count;favorite_count");
+    }
+
 
     public static void writeLine(PrintStream out) {
         if (candidate == " ") {
@@ -102,18 +112,23 @@ public class DatenBereinigung {
             out.print(";");
             out.print(text);
             out.print(";");
+            out.print(isRetweet);
+            out.print(";");
+            out.print(originalAuthor);
+            out.print(";");
             if (hashtags.size() != 0) {
                 for (int i = 0; i < hashtags.size() - 1; i++) {
                     String hashtag = hashtags.get(i);
                     out.print(hashtag);
-                    out.print(",");
+                    out.print("+");
                 }
                 out.print(hashtags.get(hashtags.size() - 1));
                 out.print(";");
             }
+            else {
+                out.print(";");
+            }
             out.print(time);
-            out.print(";");
-            out.print(quote);
             out.print(";");
             out.print(retweet);
             out.print(";");
@@ -137,16 +152,20 @@ public class DatenBereinigung {
             PrintStream out = new PrintStream("american-election-tweets-clean.csv");
             String l;
 
+            writeHeader(out);
+
 
             while ((l = reader.readLine()) != null) {
                 String[] ll = l.split(";");
+
 
                 if (ll.length == 11) {
 
                     checkCandidate(ll[0]);
                     checkTweet(ll[1]);
                     checkTime(ll[4]);
-                    checkQuote(ll[6]);
+                    checkOriginal(ll[3]);
+                    checkIsRetweet(ll[2]);
                     checkRetweetCount(ll[7]);
                     checkFavoriteCount(ll[8]);
                     getHashtag(ll[1]);
@@ -155,6 +174,41 @@ public class DatenBereinigung {
 
 
                 }
+
+                /*else {
+                    damaged_line += l.replace("/n"," ");
+                    String[] split = damaged_line.split(";");
+
+                    if (split.length == 11) {
+                        checkCandidate(ll[0]);
+                        checkTweet(ll[1]);
+                        checkTime(ll[4]);
+                        checkOriginal(ll[3]);
+                        checkIsRetweet(ll[2]);
+                        checkRetweetCount(ll[7]);
+                        checkFavoriteCount(ll[8]);
+                        getHashtag(ll[1]);
+
+                        writeLine(out);
+
+                        damaged_line = "";
+
+                    }
+                }*/
+
+                /*else if (ll.length != 11){
+                    if (ll[1].contains("\n")) {
+                        String fix = ll[1].replace("\n"," ");
+                        if (damaged_line == "") {
+                            damaged_line = damaged_line + l;
+                        }
+                        else {
+                            damaged_line = damaged_line + ";" + l;
+                        }
+
+                    }
+                }*/
+
 
 
             }
